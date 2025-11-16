6c912c0c60e8f0ea60564cd7683d5ff8a8ca57d3
Eskinder <eskinderget@gmail.com>
2025-11-16 14:32:51 +0300

    number

--- src/main/java/com/project/api/model/Note.java
+++ src/main/java/com/project/api/model/Note.java
@@ -50,7 +50,7 @@ public class Note {
 
     @DatabaseGeneratedValue
     @Column(name = "pin_order", nullable = false)
-    private Timestamp pin_order = new Timestamp(new Date().getTime());
+    private Number pin_order;
 
     @CreationTimestamp
     @Column(name = "date_created", insertable = false, nullable = false)
@@ -158,11 +158,11 @@ public class Note {
         this.spell_check = spell_check;
     }
 
-    public Timestamp getPin_order() {
+    public Number getPin_order() {
         return pin_order;
     }
 
-    public void setPin_order(Timestamp pin_order) {
+    public void setPin_order(Number pin_order) {
         this.pin_order = pin_order;
     }
 
