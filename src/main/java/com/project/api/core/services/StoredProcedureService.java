97e5bc58b80c126e4807c14065570a9c358bdd43
Eskinder <eskinderget@gmail.com>
2025-12-30 17:55:45 +0300

    col mapper

--- src/main/java/com/project/api/core/services/StoredProcedureService.java
+++ src/main/java/com/project/api/core/services/StoredProcedureService.java
@@ -1,14 +1,11 @@
 package com.project.api.core.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,21 +19,10 @@ public class StoredProcedureService {
     public List<Map<String, Object>> callProcedureForList(String procedureName, Map<String, Object> inParams) {
         SimpleJdbcCall jdbcCall = new SimpleJdbcCall(dataSource)
                 .withProcedureName(procedureName)
-                .returningResultSet("result_set_name", new GenericMapRowMapper());
+                .returningResultSet("result_set_name", new ColumnMapRowMapper());
 
         Map<String, Object> result = jdbcCall.execute(inParams); // Execute the stored procedure
 
         return (List<Map<String, Object>>) result.get("result_set_name");
-    }
-
-    public static class GenericMapRowMapper implements RowMapper<Map<String, Object>> {
-        @Override
-        public Map<String, Object> mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Map<String, Object> row = new HashMap<>();
-            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-                row.put(rs.getMetaData().getColumnName(i), rs.getObject(i));
-            }
-            return row;
-        }
     }
 }
