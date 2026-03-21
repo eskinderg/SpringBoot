ac3ec6825a8a6a41a6f49244c95ef44f6ef9046e
Eskinder <eskinderget@gmail.com>
2026-03-21 15:19:25 +0300

    comment version

--- src/main/java/com/project/api/model/Note.java
+++ src/main/java/com/project/api/model/Note.java
@@ -81,12 +81,12 @@ public class Note {
     @Column(name = "text", length = 16777215, columnDefinition = "mediumtext")
     private String text;
 
-    @Column(name = "version_id")
-    private long version_id;
+//    @Column(name = "version_id")
+//    private long version_id;
 
-    public long getVersion_id(){ return version_id; };
-
-    public void setVersion_id(long version_id) { this.version_id = version_id; };
+//    public long getVersion_id(){ return version_id; };
+//
+//    public void setVersion_id(long version_id) { this.version_id = version_id; };
 
     public UUID getNote_id() {
         return note_id;
